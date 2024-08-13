5b9cf65fe9db5688751d2fe65a4ecfa62c215aa7
Paul <paul.borjesson.sesma@gmail.com>
2024-08-13 20:25:50 +0200

    adding Json parsin

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	cyoa "github.com/Paul3435/CYOAdventure"
+)
 
 func main() {
-	fmt.Println("hola")
+	//Create flag for file to use
+	fileLocation := flag.String("file", "stories.json", "Specify the file in which you want to run this program (Default: stories.json)")
+	flag.Parse()
+
+	//Read the file
+	file := readFile(*fileLocation)
+
+	story, err := cyoa.JsonReader(file)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", story)
+
+}
+
+func readFile(fileLocation string) *os.File {
+	f, err := os.Open(fileLocation)
+	if err != nil {
+		panic(err)
+	}
+
+	return f
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Paul3435/CYOAdventure
+
+go 1.22.5
